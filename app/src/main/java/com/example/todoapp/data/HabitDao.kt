795220e96cbfdea5a0ee9857e22d66fdc9177397
kaihitwari1996-ui <kaihitwari1996@gmail.com>
2026-08-05 08:55:34 +0530

    Update HabitDao.kt

--- app/src/main/java/com/example/todoapp/data/HabitDao.kt
+++ app/src/main/java/com/example/todoapp/data/HabitDao.kt
@@ -7,6 +7,7 @@ import com.example.todoapp.data.entities.HabitEntry
 
 @Dao
 interface HabitDao {
+
     @Query("SELECT * FROM habits")
     fun getAllHabits(): Flow<List<Habit>>
 
@@ -16,6 +17,12 @@ interface HabitDao {
     @Delete
     suspend fun deleteHabit(habit: Habit)
 
+    @Query("SELECT * FROM habit_entries WHERE habitId = :habitId")
+    fun getEntriesForHabit(habitId: Int): Flow<List<HabitEntry>>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertHabitEntry(entry: HabitEntry)
+
+    @Delete
+    suspend fun deleteHabitEntry(entry: HabitEntry)
 }
