599bfa5adf15cd71cfb219524830b1782f90b73c
kaihitwari1996-ui <kaihitwari1996@gmail.com>
2026-08-05 11:56:17 +0530

    Update HabitScreen.kt

--- app/src/main/java/com/example/todoapp/ui/screens/HabitScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/screens/HabitScreen.kt
@@ -1,5 +1,5 @@
 package com.example.todoapp.ui.screens
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
